30073f356afcf56aa250c5a60e95078d2722ab14
Viktor <102484631+WEWEtya@users.noreply.github.com>
2023-05-27 13:34:22 +0300

    Update Functoins.cpp

--- Functoins.cpp
+++ Functoins.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int Sum(int a, int b)
 {
-    int c = a + b;
+    int Result = a + b;
 
-    return c;
+    return Result;
 }
 
 int main()
 {
     int d =4;
     int q = 4;
-    int v = Sum(q, d);
+    int Result = Sum(q, d);
 
-    cout << v << endl;
+    cout << Result << endl;
 
 }
